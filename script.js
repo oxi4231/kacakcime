9f05f1a5ddd16374d6649675d2eb4ff087d29498
oxi4231 <akropavic@gmail.com>
2025-04-17 17:49:23 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('#stream-menu button');
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      const streamPage = button.getAttribute('data-stream');
-      window.location.href = streamPage;
-    });
-  });
-});
